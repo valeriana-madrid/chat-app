07808d10d77b882dcf0754e8c8d726f98c0793c4
valeriana <madridjmvalerie@gmail.com>
2024-09-20 13:01:54 +0800

    FIX CODE

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material'
-import { pink } from '@mui/material/colors'
+import { blue } from '@mui/material/colors'
 import React, { useEffect, useState } from 'react'
 import ChatArea from '../components/chat/ChatArea'
 import ListOfUsers from '../components/chat/ListOfUsers'
 import Welcome from '../components/chat/Welcome'
-import { useUser } from '../contexts/UserContext'
 import api from '../configs/api'
+import { useUser } from '../contexts/UserContext'
 
 const Chats = () => {
 
@@ -20,7 +20,7 @@ const Chats = () => {
     }, [])
 
     return (
-        <Box sx={{ background: pink[100], height: '80vh', mt: 5, width: '80%', mx: 'auto', display: 'flex' }}>
+        <Box sx={{ background: blue[100], height: '80vh', mt: 5, width: '80%', mx: 'auto', display: 'flex' }}>
             {/* list of users */}
             <ListOfUsers users={users} setSelectedUser={setSelectedUser} />
 
